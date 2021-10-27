913e67c4a13ccf4ab387c005408f220dcafa57fe
Jake Lindvall <jakeandbakelindvall@gmail.com>
2021-10-26 22:38:36 -0600

    component structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import './index.css';
+import "./index.css";
 
-import App from './App';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import App from "./components/App";
+import React from "react";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.querySelector('#app')
+  document.querySelector("#app")
 );
